c4d3da2ea52e2fa9978adb334fdaca7f4cf80010
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2023-03-26 16:14:16 +0200

    fix terraform validate errors

--- create-vm/outputs.tf
+++ create-vm/outputs.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.myRG.name
 }
 
 output "public_ip_address" {
@@ -7,6 +7,6 @@ output "public_ip_address" {
 }
 
 output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
+  value     = tls_private_key.myPrivateKey.private_key_pem
   sensitive = true
 }
